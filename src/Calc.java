4702629ce74833aa1261e750d6d2bd582fb745db
dkbull7 <den.kovalchuk.2013@mail.ru>
2017-03-14 21:12:07 +0200

    java commit

---
+++ src/Calc.java
@@ -0,0 +1,21 @@
+
+public class Calc {
+
+	  
+public int sum(int a, int b){
+	return a+b;
+}
+	  
+public int dif(int a, int b){
+	return a-b;
+}
+
+public int calc(int a, int b){
+	return a*b;
+}
+
+public int dil(int a, int b){
+	return a/b;
+}
+
+}
---
+++ src/Dispatcher.java
@@ -0,0 +1,14 @@
+
+public class Dispatcher {
+
+	public static void main(String[] args) {
+
+		Calc calculator = new Calc();
+		System.out.println(calculator.sum(10, 15));
+		System.out.println(calculator.dif(10, 15));
+		System.out.println(calculator.calc(10, 15));
+		System.out.println(calculator.dil(100, 10));
+		
+	}
+
+}
